5b9ee2a87b3435ccc9a072cb02f5c6f2b99ddc39
Ryan Jordan <ryjordan@gmail.com>
2023-09-06 08:57:14 -0500

    first commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,55 +1,55 @@
-"use client";
-import { Github, Mail, Twitter } from "lucide-react";
-import Link from "next/link";
-import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
+'use client'
+import { Github, Mail, Twitter } from 'lucide-react'
+import Link from 'next/link'
+import { Navigation } from '../components/nav'
+import { Card } from '../components/card'
 
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		href: 'https://twitter.com/ryanjordandev',
+		label: 'Twitter',
+		handle: '@ryanjordandev'
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
-		label: "Email",
-		handle: "dev@chronark.com",
+		href: 'mailto:ryjordan@gmail.com',
+		label: 'Email',
+		handle: 'ryjordan@gmail.com'
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
-		label: "Github",
-		handle: "chronark",
-	},
-];
+		href: 'https://github.com/krjordan',
+		label: 'Github',
+		handle: 'krjordan'
+	}
+]
 
 export default function Example() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className=' bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0'>
 			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-					{socials.map((s) => (
+			<div className='container flex items-center justify-center min-h-screen px-4 mx-auto'>
+				<div className='grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16'>
+					{socials.map(s => (
 						<Card>
 							<Link
 								href={s.href}
-								target="_blank"
-								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+								target='_blank'
+								className='p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16'
 							>
 								<span
-									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-									aria-hidden="true"
+									className='absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent'
+									aria-hidden='true'
 								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+								<span className='relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange'>
 									{s.icon}
-								</span>{" "}
-								<div className="z-10 flex flex-col items-center">
-									<span className="text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+								</span>{' '}
+								<div className='z-10 flex flex-col items-center'>
+									<span className='text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display'>
 										{s.handle}
 									</span>
-									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+									<span className='mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200'>
 										{s.label}
 									</span>
 								</div>
@@ -59,5 +59,5 @@ export default function Example() {
 				</div>
 			</div>
 		</div>
-	);
+	)
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
-import "../global.css";
-import { Inter } from "@next/font/google";
-import LocalFont from "@next/font/local";
-import { Metadata } from "next";
-import { Analytics } from "./components/analytics";
+import '../global.css'
+import { Inter } from '@next/font/google'
+import LocalFont from '@next/font/local'
+import { Metadata } from 'next'
+import { Analytics } from './components/analytics'
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: 'chronark.com',
+		template: '%s | chronark.com'
 	},
-	description: "Software engineer at upstash.com and founder of planetfall.io",
+	description:
+		"A decade-plus in web dev and I still get excited about a clean console. Angular's my old pal, Next.js is the cool kid, and I'm just here making magic and mischief!",
 	openGraph: {
-		title: "chronark.com",
-		description:
-			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
+		title: 'ryanjordan.me',
+		description: `A decade-plus in web dev and I still get excited about a clean console. Angular's my old pal, Next.js is the cool kid, and I'm just here making magic and mischief!`,
+		url: 'https://ryanjordan.me',
+		siteName: 'ryanjordan.me',
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: 'https://chronark.com/og.png',
 				width: 1920,
-				height: 1080,
-			},
+				height: 1080
+			}
 		],
-		locale: "en-US",
-		type: "website",
+		locale: 'en-US',
+		type: 'website'
 	},
 	robots: {
 		index: true,
@@ -32,46 +32,49 @@ export const metadata: Metadata = {
 		googleBot: {
 			index: true,
 			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1
+		}
 	},
 	twitter: {
-		title: "Chronark",
-		card: "summary_large_image",
+		title: 'ryanjordandev',
+		card: 'summary_large_image'
 	},
 	icons: {
-		shortcut: "/favicon.png",
-	},
-};
+		shortcut: '/favicon.png'
+	}
+}
 const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
-});
+	subsets: ['latin'],
+	variable: '--font-inter'
+})
 
 const calSans = LocalFont({
-	src: "../public/fonts/CalSans-SemiBold.ttf",
-	variable: "--font-calsans",
-});
+	src: '../public/fonts/CalSans-SemiBold.ttf',
+	variable: '--font-calsans'
+})
 
 export default function RootLayout({
-	children,
+	children
 }: {
-	children: React.ReactNode;
+	children: React.ReactNode
 }) {
 	return (
-		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+		<html
+			lang='en'
+			className={[inter.variable, calSans.variable].join(' ')}
+		>
 			<head>
 				<Analytics />
 			</head>
 			<body
 				className={`bg-black ${
-					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+					process.env.NODE_ENV === 'development' ? 'debug-screens' : undefined
 				}`}
 			>
 				{children}
 			</body>
 		</html>
-	);
+	)
 }
